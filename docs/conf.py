8b073927c6bda62310253a0be55c5c0c4ce8dad6
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-01-18 09:33:08 +0100

    Always use sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -88,10 +88,11 @@ todo_include_todos = False
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#if not on_rtd:  # only import and set the theme if we're building docs locally
+
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # explicitly define
 html_context = {
